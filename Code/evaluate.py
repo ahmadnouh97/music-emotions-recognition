db694068cec8098740af4e5f5deb3ea217532561
Ahmad Nouh <ahmadnouh428@gmail.com>
2020-12-28 20:18:24 +0200

    start valence_experiment

--- Code/evaluate.py
+++ Code/evaluate.py
@@ -1,33 +1,31 @@
-from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_squared_log_error
+from keras.models import Model, load_model
+import keras.metrics as metrics
 from pathlib import Path
 import sys
 import json
 import yaml
-import pickle as pkl
 
 sys.path.append(str(Path.absolute(self=Path('./'))))
 from Code.config import Config
 from Code.helper import read_data
+from Code.train import create_model
 
 METRICS_PATH = Path(sys.argv[1])
 params = yaml.safe_load(open(Config.PARAMS_PATH))['train']
 
 
-def evaluate(x_test, y_test):
-    with open(str(Config.MODEL_PATH / 'model.pkl'), 'rb') as file:
-        model = pkl.load(file)
-
-    predictions = model.predict(x_test)
-    rmsle = mean_squared_log_error(y_true=y_test, y_pred=predictions)
-    rmse = mean_squared_error(y_true=y_test, y_pred=predictions)
-    mae = mean_absolute_error(y_true=y_test, y_pred=predictions)
-
+def evaluate(model: Model, x_test, y_test):
+    results = model.evaluate(x_test, y_test)
     with open(METRICS_PATH, 'w') as file:
         json.dump({
-            'RMSE': rmse,
-            'RMSLE': rmsle,
-            'MAE': mae
+            'Loss': results[0],
+            'RMSE': results[1],
+            'RMSLE': results[2],
+            'MAE': results[3]
         }, file)
+
+    print(f'results = {results}')
+    predictions = model.predict(x_test)
 
     print(f'predictions = {predictions[:10]}')
     print(f'y_true = {y_test[:10]}')
@@ -35,7 +33,15 @@ def evaluate(x_test, y_test):
 
 def main():
     _, x_test, _, y_test = read_data()
-    evaluate(x_test, y_test)
+    features_num = x_test.shape[1]
+    model = create_model(
+        features_num,
+        learning_rate=float(params['learning_rate']),
+        regularization_factor=float(params['regularization_factor']),
+        hidden_units=params['hidden_units']
+    )
+    model.load_weights(str(Config.MODEL_PATH / 'model.h5'))
+    evaluate(model, x_test, y_test)
 
 
 main()
